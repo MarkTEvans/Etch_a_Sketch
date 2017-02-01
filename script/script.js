7353178f1848ec9521746a8bcbdef5c412679f05
Mark Evans <mark.timothy.evans@gmail.com>
2017-01-31 22:14:27 -0500

    Updated to include jQuery and modified board size

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
-$(document).onready(function() {
+$(document).ready(function() {
 var col = prompt('How many rows/columns would you like?');
 var row = col;
-var size = 350/col;
+var size = 600/col;
 var container = $('.container');
 
 for (var r = 0; r < row; r++) {
@@ -17,15 +17,10 @@ $(pixel).width(size).height(size);
 
 $(pixel).mouseenter(function() {
   $(this).addClass('filled');
-  $(this).removeClass('pixel');
 });
 
 $('#clear').on('click', function() {
-	$('.filled').fadeOut('slow', function() {
-  	$('.filled').addClass('pixel');
-    $(pixel).css('display','block');
-    $('.filled').removeClass('filled');
-  });
+  $('.filled').removeClass('filled');
 });
 
 $('#newsize').on('click', function() {
